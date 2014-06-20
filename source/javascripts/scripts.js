8d19e6b40e41be996b077af4f917a5b3ccc7ccf5
alauderdale <alauderdale@mac.com>
2014-06-20 10:32:14 -0600

    main stuff

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
 	 //fancybox
-	$(".fancybox").fancybox();
+	$('.fancybox').fancybox();
 	
 	$('.fb-video').fancybox({
         padding: 0,
         helpers: {
             media: {}
         }
-    });
+   });
 
 });
