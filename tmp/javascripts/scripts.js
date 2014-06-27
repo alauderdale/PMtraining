03106e96c764df79b3af71dd144c415f2183aa9d
Alex Lauderdale <alauderdale@mac.com>
2014-06-27 08:59:13 -0600

    build

--- tmp/javascripts/scripts.js
+++ tmp/javascripts/scripts.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
 
 	 //fancybox
-	$(".fancybox").fancybox();
+	$('.fancybox').fancybox();
 	
 	$('.fb-video').fancybox({
         padding: 0,
         helpers: {
             media: {}
         }
-    });
+   });
+
+	$('.tooltip-up').tooltip();
+
+	///responsive tables!!!
+	$('table').footable();
+
+	$('#testimonial-slider .item:first-child').addClass('active');
+
+	$('.carousel').carousel({
+  	interval: 4000
+	})
 
 });
